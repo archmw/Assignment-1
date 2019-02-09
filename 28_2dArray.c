85f0953e0e8432631537c5ab5ca9d5050dffe810
archmw-admin-PC <arch.wadhwani@gmail.com>
2019-02-09 22:33:52 +0530

    task 28 2D array memory allocation.

---
+++ 28_2dArray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+extern int *arr;
+void set2DArray(int row, int col){
+    int count = 0;
+    int *arr = (int *)malloc(row * col * sizeof(int));
+    for(int i = 0; i<row;i++){
+        for(int j = 0; j<col;j++){
+            *(arr+i*col+j) = ++count;
+        }
+    }    
+    for(int i = 0; i<row;i++){
+        for(int j = 0; j<col;j++){
+            printf("Arr[%d][%d] = %d, ",i,j,*(arr+i*col+j));
+        }
+        printf("\n");
+    }
+    
+}
+
+
+int main() {
+	//code
+	set2DArray(4,3);
+	return 0;
+}
